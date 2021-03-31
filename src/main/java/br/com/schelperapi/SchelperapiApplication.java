735eea6e4ecd5baaf5f01606841977347206d830
gabriel <gabrielalmeidads@gmail.com>
2021-03-30 22:52:01 -0300

    Adicionando projeto

---
+++ src/main/java/br/com/schelperapi/SchelperapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.schelperapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchelperapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchelperapiApplication.class, args);
+	}
+
+}
